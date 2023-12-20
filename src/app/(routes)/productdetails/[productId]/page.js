8f03745cca8cb88802dca8829c5388e57b403c72
Vishal Krishna <121739011+vishalkrishna8@users.noreply.github.com>
2023-12-20 18:02:38 +0530

    Update page.js

--- src/app/(routes)/productdetails/[productId]/page.js
+++ src/app/(routes)/productdetails/[productId]/page.js
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import ProductDetails from "@/app/Features/ProductDetails/components/ProductDetails";
 function Page({ params }) {
   const router = useRouter();
   console.log(params.productId);
